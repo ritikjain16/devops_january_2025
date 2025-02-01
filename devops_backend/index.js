4bf6046d7b6fcc096ae35b309e1eb4ea88acd282
Ritik Jain <ritik9628@gmail.com>
2025-02-01 10:05:39 +0530

    devops_commit

---
+++ devops_backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const port = process.env.PORT | 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send({ msg: "Hello From Devops Backend" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
